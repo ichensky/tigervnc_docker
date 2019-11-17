6f2e263ea40a7c034f4b3a7d829cdda124350f23
ichensky <IChensky@live.com>
2019-11-17 16:08:32 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,1 @@
-sudo docker run ichensky/tigervnc:ctrl_r > tmp.deb && sudo dpkg -i tmp.deb && rm tmp.deb
+sudo docker run ichensky/tigervnc:ctrl_r > tmp.deb && sudo dpkg -i tmp.deb && sudo rm tmp.deb
